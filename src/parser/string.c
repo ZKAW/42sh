e8f75e35ae65ff7e808876c82ff58b3e5bab1463
ZKAW <zkaw.exod@gmail.com>
2023-05-02 22:39:59 +0200

    chore(parser): libmy > libc

--- src/parser/string.c
+++ src/parser/string.c
@@ -9,16 +9,18 @@
 
 char **tokenize_string(char *input, char *sep)
 {
-    char *input_dup = my_strdup(input);
-    char **parsed = malloc(sizeof(char *) * (my_strlen(input_dup) + 1));
+    char *input_dup = strdup(input);
+    char **parsed = malloc(sizeof(char *) * (strlen(input_dup) + 1));
     int i = 0;
     char *token = strtok(input_dup, sep);
 
     while (token != NULL) {
-        parsed[i] = clean_str(my_strdup(token));
+        parsed[i] = clean_str(strdup(token));
         token = strtok(NULL, sep);
         i++;
     }
     parsed[i] = NULL;
+    free(input_dup);
+
     return (parsed);
 }
