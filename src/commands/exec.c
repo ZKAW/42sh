d3519e22fc4f4024e5d1b349bc50e3289a82c002
jademagnin <jade.magnin1@gmail.com>
2023-05-10 02:17:09 +0200

    feat(alias): replace alias

--- src/commands/exec.c
+++ src/commands/exec.c
@@ -41,40 +41,73 @@ int assign_variables(cmd_t* cmd, shell_t* shell)
     return 0;
 }
 
-char* get_alias(shell_t* shell, char* key)
+
+////////////////////////
+
+
+char *get_alias(shell_t *shell, char *key)
 {
+    if (shell->aliases->alias == NULL)
+        return NULL;
     for (int i = 0; shell->aliases->next != NULL; i++) {
-        if (my_strcmp(shell->aliases->command, key) == 0)
-            return shell->aliases->alias;
+        if (my_strcmp(shell->aliases->alias, key) == 0)
+            return shell->aliases->command;
         shell->aliases = shell->aliases->next;
-        }
-    if (my_strcmp(shell->aliases->command, key) == 0) {
-        printf("%s\n", shell->aliases->alias);
-        return shell->aliases->alias;
     }
+    if (my_strcmp(shell->aliases->alias, key) == 0)
+        return shell->aliases->command;
     return NULL;
+}
+
+int my_array_len(char** array)
+{
+    int i = 0;
+    for (; array[i] != NULL; i++);
+    return i;
+}
+
+void insert_str_to_array(char** array, char* str, int index, cmd_t* cmd)
+{
+    char** new_array = malloc(sizeof(char*) * (my_array_len(array) + 1));
+    int i = 0;
+    for (; i < index; i++)
+        new_array[i] = array[i];
+    char** splitted = my_str_to_word_array(str, ' ');
+    for (int j = 0; splitted[j] != NULL; j++) {
+        new_array[i] = splitted[j];
+        i++;
+    }
+    for (; array[i + 1] != NULL; i++)
+        new_array[i + 1] = array[i + 1];
+    new_array[i + 1] = NULL;
+    for (int j = 0; splitted[j] != NULL; j++)
+        cmd->argv[j] = splitted[j];
 }
 
 int cmd_is_alias(cmd_t* cmd, shell_t* shell)
 {
     for (int i = 0; cmd->argv[i] != NULL; i++) {
         char *alias = get_alias(shell, cmd->argv[i]);
-        if (alias == NULL) {
+        if (alias != NULL) {
+            insert_str_to_array(cmd->argv, alias, i, cmd);
             return 1;
         }
-        cmd->argv[i] = alias;
-        printf("f: %s\n", cmd->argv[i]);
     }
     return 0;
 }
 
 void run_command(cmd_t* cmd, shell_t* shell)
 {
+    // for (int i = 0; cmd->argv[i] != NULL; i++) {
+    //     printf("%s\n", cmd->argv[i]);
+    // }
     int fd[2];
     if(assign_variables(cmd, shell) == 1)
         return;
-    // if(cmd_is_alias(cmd, shell) == 1)
-    //     return;
+    cmd_is_alias(cmd, shell);
+    for (int i = 0; cmd->argv[i] != NULL; i++) {
+        printf("%s\n", cmd->argv[i]);
+    }
     char* path = cmd->argv[0];
     char *full_path = get_full_path(cmd->argv[0], shell);
 
