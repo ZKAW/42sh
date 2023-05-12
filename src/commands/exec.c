df75581e9224fc68d96767fa433b5b40c9f1cc36
mezz <mohamed.mazouz@epitech.eu>
2023-05-12 18:11:26 +0200

    feat(globbing): multiple path globbing handling

--- src/commands/exec.c
+++ src/commands/exec.c
@@ -12,7 +12,10 @@ void sigchld_handler(int signo);
 
 void teach_child(char* path, cmd_t *cmd, shell_t* shell)
 {
-    if (check_globbing(cmd, shell) == 1) exit(1);
+    if (check_globbing(cmd, shell) == 1) {
+        SHARED_STATUS = 1;
+        exit(1);
+    }
     if (is_builtin(cmd->argv[0])) {
         run_builtin(cmd, shell);
     }
--- src/globbing/replacing_core.c
+++ src/globbing/replacing_core.c
@@ -22,7 +22,6 @@ char* get_globbing_path(globber_t** globbing)
     }
     strcat(path, "./");
     strcat(path, globber->value);
-    *globbing = globber->next;
     return strdup(path);
 }
 
@@ -41,17 +40,6 @@ int match(char* str, globber_t* globbing)
     return 1;
 }
 
-char* set_file_path(char* path, char* file)
-{
-    char full_path[4096] = {0};
-    if (my_strncmp(path, "./", 2) == 0)
-        strcat(full_path, path + 2);
-    else
-        strcat(full_path, path);
-    strcat(full_path, file);
-    return strdup(full_path);
-}
-
 char** get_matching_files(char** files, char* path, globber_t* globbing)
 {
     char full_files[4096] = {0};
@@ -59,20 +47,26 @@ char** get_matching_files(char** files, char* path, globber_t* globbing)
     int position = 0;
     for (int i = 0; files[i]; i++)
         if (match(files[i], globbing))
-            matched[position++] = set_file_path(path, files[i]);
+            matched[position++] = files[i];
     matched[position] = NULL;
     return matched;
 }
+
+char** get_recursive_files(char* root);
+
 int replace_globber(globber_t* globbing, int index, cmd_t* cmd)
 {
     shell_t* shell = get_shell(NULL);
     char* path = get_globbing_path(&globbing);
-    char** files = get_files(path);
+    char** files = get_recursive_files(path);
+    //print_array(files);
     char** matched = get_matching_files(files, path, globbing);
+    //print_array(matched);
     replace_arr_at_index(&cmd->argv, matched, index);
     if (tablen(matched) == 0) {
         dprintf(2, "%s: No match.\n", cmd->argv[0]);
         SHARED_STATUS = 1;
+        exit(1);
         return 0;
     }
     return tablen(matched) - 1;
