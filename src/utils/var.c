01ad59d558dfb479d574ef1c644e7ed8498950bc
jademagnin <jade.magnin1@gmail.com>
2023-05-14 18:34:06 +0200

    feat(set): sorted by ascii

--- src/utils/var.c
+++ src/utils/var.c
@@ -7,6 +7,40 @@
 
 #include "mysh.h"
 
+void swap(var_t *a, var_t *b);
+
+void bubble_sort(var_t *set)
+{
+    int swapped;
+    var_t *ptr1;
+    var_t *lptr = NULL;
+    if (set == NULL)
+        return;
+    do {
+        swapped = 0;
+        ptr1 = set;
+
+        while (ptr1->next != lptr) {
+            if (strcmp(ptr1->key, ptr1->next->key) > 0) {
+                swap(ptr1, ptr1->next);
+                swapped = 1;
+            }
+            ptr1 = ptr1->next;
+        }
+        lptr = ptr1;
+    } while (swapped);
+}
+
+void swap(var_t *a, var_t *b)
+{
+    char *tmp_key = a->key;
+    char *tmp_value = a->value;
+    a->key = b->key;
+    a->value = b->value;
+    b->key = tmp_key;
+    b->value = tmp_value;
+}
+
 char* array_to_str(char **cmd)
 {
     char *tmp = malloc(sizeof(char) * 500);
