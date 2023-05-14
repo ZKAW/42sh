45d12a9f128f43ed30e12cfd15425a5b004ae6b2
ZKAW <zkaw.exod@gmail.com>
2023-05-14 03:09:51 +0200

    refactor(builtin): cd architecture

--- src/commands/builtins/cd.c
+++ src/commands/builtins/cd.c
@@ -23,25 +23,38 @@ static void exec_cwdcmd(shell_t* shell)
     }
 }
 
-void builtin_cd(BUILTIN_PARAMS)
+static void change_dir(char *path, shell_t *shell)
 {
-    static char *old_pwd = NULL;
+    if (chdir(path) < 0) {
+        handle_cd_error(path, shell);
+    }
+    exec_cwdcmd(shell);
+}
+
+static char *get_path(cmd_t *cmd, shell_t *shell, char *old_pwd)
+{
     char *path = cmd->argv[1];
 
-    if (tablen(cmd->argv) > 2) {
-        throw_error("cd: Too many arguments.\n", shell, 1);
-        return;
-    }
-    if (old_pwd == NULL) old_pwd = getcwd(NULL, 0);
     if (cmd->argv[1] == NULL || is_whitespace(cmd->argv[1])
         || strcmp(cmd->argv[1], "~") == 0) {
         path = get_env_var(shell->envp, "HOME");
     } else if (strcmp(path, "-") == 0) {
         path = old_pwd;
     }
-    old_pwd = getcwd(NULL, 0);
-    if (chdir(path) < 0) {
-        handle_cd_error(path, shell);
+
+    return path;
+}
+
+void builtin_cd(BUILTIN_PARAMS)
+{
+    static char *old_pwd = NULL;
+
+    if (tablen(cmd->argv) > 2) {
+        throw_error("cd: Too many arguments.\n", shell, 1);
+        return;
     }
-    exec_cwdcmd(shell);
+    if (old_pwd == NULL) old_pwd = getcwd(NULL, 0);
+    char *path = get_path(cmd, shell, old_pwd);
+    old_pwd = getcwd(NULL, 0);
+    change_dir(path, shell);
 }
