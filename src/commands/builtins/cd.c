0f62d154c17afd18f7bd7303fc8df7eafbda7602
Louis Lafosse <louis.lafosse@epitech.eu>
2023-05-14 02:58:56 +0200

    fix(echo) : fix echo and implement him again with optimisations

--- src/commands/builtins/cd.c
+++ src/commands/builtins/cd.c
@@ -34,8 +34,7 @@ void builtin_cd(BUILTIN_PARAMS)
     char actual_path[500], **var_env = shell->envp, *dir, *var;
     if (tablen(cmd) > 2) {
         throw_error("cd: Too many arguments.\n", shell, 1); return;
-    }
-    dir = (tablen(cmd) == 1 || !strcmp(cmd[1], "~")) ? "$HOME" : cmd[1];
+    } dir = (tablen(cmd) == 1 || !strcmp(cmd[1], "~")) ? "$HOME" : cmd[1];
     if (dir[0] == '$') {
         var = find_envp(dir, shell); var_env = tokenize_string(var, "=");
         dir = var_env[1];
